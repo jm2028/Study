254e597f3965eeffd07e4a8f7f432108469c672f
JongMin <rlawhdals228@gmail.com>
2023-01-18 23:58:52 +0900

    Part.2 라우팅

--- CodingApple/React/shop/src/App.js
+++ CodingApple/React/shop/src/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
-import 슈즈1 from './img/shoes1.jpg';
+import 데이터 from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detail.js'
 
 function App() {
+  let [shoes] = useState(데이터);
+
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Link to='/'>홈</Link>
+          <Link to='/detail'>디테일</Link>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -16,28 +22,41 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg' style={{ backgroundImage : `url(${슈즈1})`}}></div>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-md-4'>
-            <img src={`${슈즈1}`} width='80%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoes2.jpg'} width='80%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-          <div className='col-md-4'>
-            <img src={process.env.PUBLIC_URL + '/shoes3.jpg'} width='80%'/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </div>
-        </div>
-      </div>
+
+      <Routes>
+          <Route path='/' element={
+            <>
+            <div className='main-bg' style={{ backgroundImage : `url(${process.env.PUBLIC_URL + '/shoes1.jpg'})`}}></div>
+            <div className='container'>
+              <div className='row'>
+                {
+                  shoes.map((data, idx)=>{
+                    return <Item shoes={data} key={idx}></Item>;
+                  })
+                }
+              </div>
+            </div>
+            </>
+          }/>
+          <Route path='/detail' element={<Detail/>}/>
+      </Routes>
+    </div>                    
+  );
+}
+
+function Item (props) {
+  return (
+    <div className='col-md-4'>
+      <img src={props.shoes.src} alt={props.shoes.title} width='80%' />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
 
 export default App;
+
+/** Route
+ * Route 안에 Routes로 페이지 생성 가능
+ * <Link>로 해당 페이지로 이동 가능
+ */
