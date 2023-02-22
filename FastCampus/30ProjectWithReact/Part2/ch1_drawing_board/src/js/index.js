1ffa81aae1d8e9d6ee7897678a115ee1719f08bc
JongMin <rlawhdals228@gmail.com>
2023-02-23 01:04:38 +0900

    nodemodule 삭제

--- FastCampus/30ProjectWithReact/Part2/ch1_drawing_board/src/js/index.js
+++ FastCampus/30ProjectWithReact/Part2/ch1_drawing_board/src/js/index.js
@@ -1,6 +1,6 @@
-console.log("그림판");
+console.log('그림판');
 class DrawingBoard {
-  MODE = "NONE"; //NONE BRUSH ERASER
+  MODE = 'NONE'; //NONE BRUSH ERASER
   IsMouseDown = false;
   constructor() {
     this.assignElement();
@@ -9,37 +9,33 @@ class DrawingBoard {
   }
 
   assignElement() {
-    this.containerEl = document.getElementById("container");
-    this.canvasEl = this.containerEl.querySelector("#canvas");
-    this.toolbarEl = this.containerEl.querySelector("#toolbar");
-    this.brushEl = this.containerEl.querySelector("#brush");
-    this.colorPicker = this.toolbarEl.querySelector("#colorPicker");
-    this.brushPanelEl = this.containerEl.querySelector("#brushPanel");
-    this.brushSlideEl = this.brushPanelEl.querySelector("#brushSize");
+    this.containerEl = document.getElementById('container');
+    this.canvasEl = this.containerEl.querySelector('#canvas');
+    this.toolbarEl = this.containerEl.querySelector('#toolbar');
+    this.brushEl = this.containerEl.querySelector('#brush');
+    this.colorPicker = this.toolbarEl.querySelector('#colorPicker');
+    this.brushPanelEl = this.containerEl.querySelector('#brushPanel');
+    this.brushSlideEl = this.brushPanelEl.querySelector('#brushSize');
 
-    this.brushSizePreveiewEl =
-      this.brushPanelEl.querySelector("#brushSizePreview");
+    this.brushSizePreveiewEl = this.brushPanelEl.querySelector('#brushSizePreview');
   }
 
   initContext() {
-    this.context = this.canvasEl.getContext("2d");
+    this.context = this.canvasEl.getContext('2d');
   }
 
   addEvent() {
-    this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
-    this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
-    this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
-    this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
-    this.canvasEl.addEventListener("mouseout", this.onMouseOut.bind(this));
-    this.brushSlideEl.addEventListener(
-      "input",
-      this.onChangeBrushSize.bind(this)
-    );
-    this.colorPicker.addEventListener("input", this.onChangeColor.bind(this));
+    this.brushEl.addEventListener('click', this.onClickBrush.bind(this));
+    this.canvasEl.addEventListener('mousedown', this.onMouseDown.bind(this));
+    this.canvasEl.addEventListener('mousemove', this.onMouseMove.bind(this));
+    this.canvasEl.addEventListener('mouseup', this.onMouseUp.bind(this));
+    this.canvasEl.addEventListener('mouseout', this.onMouseOut.bind(this));
+    this.brushSlideEl.addEventListener('input', this.onChangeBrushSize.bind(this));
+    this.colorPicker.addEventListener('input', this.onChangeColor.bind(this));
   }
 
   onMouseOut() {
-    if (this.MODE === "NONE") return;
+    if (this.MODE === 'NONE') return;
     this.IsMouseDown = false;
   }
 
@@ -60,17 +56,17 @@ class DrawingBoard {
   }
 
   onMouseUp(event) {
-    if (this.MODE === "NONE") return;
+    if (this.MODE === 'NONE') return;
     this.IsMouseDown = false;
   }
 
   onMouseDown(event) {
-    if (this.MODE == "NONE") return;
+    if (this.MODE == 'NONE') return;
     this.IsMouseDown = true;
     const currentPosition = this.getMousePosition(event);
     this.context.beginPath();
     this.context.moveTo(currentPosition.x, currentPosition.y);
-    this.context.lineCap = "round";
+    this.context.lineCap = 'round';
     this.context.strokeStyle = this.colorPicker.value;
     this.context.lineWidth = this.brushSlideEl.value;
     // this.context.lineTo(400, 400);
@@ -86,11 +82,11 @@ class DrawingBoard {
   }
 
   onClickBrush(event) {
-    const isActive = event.currentTarget.classList.contains("active");
-    this.MODE = isActive ? "NONE" : "BRUSH";
-    this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
-    this.brushPanelEl.classList.toggle("hide");
-    this.brushEl.classList.toggle("active");
+    const isActive = event.currentTarget.classList.contains('active');
+    this.MODE = isActive ? 'NONE' : 'BRUSH';
+    this.canvasEl.style.cursor = isActive ? 'default' : 'crosshair';
+    this.brushPanelEl.classList.toggle('hide');
+    this.brushEl.classList.toggle('active');
   }
 }
 
